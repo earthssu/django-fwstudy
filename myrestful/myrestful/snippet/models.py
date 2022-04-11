4b501259ef5f0ef950a8a17848c9aceb3fb14268
earthssu <entjq9502@khu.ac.kr>
2022-04-11 10:32:30 +0900

    Add my restful api

--- myrestful/myrestful/snippet/models.py
+++ myrestful/myrestful/snippet/models.py
@@ -3,8 +3,10 @@ from django.db import models
 
 class Snippet(models.Model):
     created = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField()
     title = models.CharField(max_length=100, blank=True, default='')
     content = models.TextField()
+
 
     class Meta:
         ordering = ['created']
